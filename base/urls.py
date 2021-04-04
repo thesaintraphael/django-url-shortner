6bd4f08948ddb0474fc695ca22c2c421995b5d3e
Rafael Salimov <raphasalimov@gmail.com>
2021-04-04 13:12:45 +0400

    url path added

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, redirect_url
+from .views import index, redirect_url, generate_url
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('<str:code>', redirect_url, name='redirect'),
+    path('generate_url', generate_url, name='generate_url'),
 ]
