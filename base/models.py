9e1c4b1f505f0f84ebec320b749f8b26d43b838a
Rafael Salimov <raphasalimov@gmail.com>
2021-04-04 13:09:29 +0400

    rest frm

--- base/models.py
+++ base/models.py
@@ -6,9 +6,12 @@ SITE_URL = 'http://127.0.0.1:8000/'
 
 
 class Link(models.Model):
-    url = models.CharField(max_length=400)
+    url = models.URLField(max_length=400)
     code = models.CharField(max_length=6, unique=True, default=uuid.uuid4().hex[:6].upper())
 
     @property
     def short_url(self):
         return SITE_URL + self.url
+
+    def __str__(self):
+        return self.code
--- base/views.py
+++ base/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
 from .models import Link
+from .serializers import LinkSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-# Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-@api_view
+@api_view(['POST'])
 def generate_url(request):
-    pass
+    serializer = LinkSerializer(request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors)
 
 
 def redirect_url(request, code):
